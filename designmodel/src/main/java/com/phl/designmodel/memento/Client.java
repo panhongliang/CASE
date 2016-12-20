7d81fedebde23c47f021d0a4dd953dde22358e81
panhongliang <panhl423@163.com>
2016-12-20 15:36:38 +0800

    增加备忘录模式

--- designmodel/src/main/java/com/phl/designmodel/memento/Client.java
+++ designmodel/src/main/java/com/phl/designmodel/memento/Client.java
@@ -6,12 +6,12 @@ package com.phl.designmodel.memento;
 public class Client {
     public static void main(String[] args) {
         Originator originator=new Originator();
-        originator.phrase1();
-        Memento memento=originator.snapshot();
+        originator.phrase1();//完成第一阶段
+        Memento memento=originator.snapshot();//创建一个快照
         Caretaker caretaker=new Caretaker();
-        caretaker.setMemento(memento);
-        originator.phrase2_m1();
-        originator.setSnapshot(memento);
-        originator.phrase2_m2();
+        caretaker.setMemento(memento);//管理者管理快照
+        originator.phrase2_m1();//用第一种办法完成第二阶段
+        originator.setSnapshot(memento);//从快照中恢复
+        originator.phrase2_m2();//恢复之后用第二种方法完成第二阶段
     }
 }
